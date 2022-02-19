d70cd6472786f6e42fd036e8906333a8a7e9cd66
Sharan1234567 <100017775+Sharan1234567@users.noreply.github.com>
2022-02-19 13:44:15 +0530

    #include<stdio.h> int main() {   int l,b,w,c,area,tarea,narea,nl,nb,cost;   scanf("%d%d%d%d",&l,&b,&w,&c);   area=l*b;   nl=l+(2*w);   nb=b+(2*w);   narea=nl*nb;   tarea=narea-area;   cost=c*tarea;   printf("%d",cost);   return 0;
    }

---
+++ Cost_to_fence_the_park_outside.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int l,b,w,c,area,tarea,narea,nl,nb,cost;
+  scanf("%d%d%d%d",&l,&b,&w,&c);
+  area=l*b;
+  nl=l+(2*w);
+  nb=b+(2*w);
+  narea=nl*nb;
+  tarea=narea-area;
+  cost=c*tarea;
+  printf("%d",cost);
+  return 0;
+  
+}
